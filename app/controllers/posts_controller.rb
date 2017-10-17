2eed6464033ed3c63013b4b9cc67b0e7edf4c4ac
Dan Engel <dengel29@gmail.com>
2017-10-17 16:15:55 +0800

    cleaning git status, still buggy

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,7 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update, :destroy]
-
+  require 'carrierwave'
+  require 'carrierwave/orm/activerecord'
   # GET /posts
   # GET /posts.json
   def index
@@ -73,6 +74,6 @@ class PostsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def post_params
-      params.require(:post).permit(:title, :content, :tag_list)
+      params.require(:post).permit(:title, :content, :tag_list, :audio)
     end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,25 @@
 class Post < ApplicationRecord
+  mount_uploader :avatar, AvatarUploader
   acts_as_taggable
+
+  TYPE_MAP = [
+    {
+      id: "text",
+      name: "text"
+    },
+    {
+      id: "audio",
+      name: "audio"
+    },
+    {
+      id: "photo",
+      name: "photo"
+    },
+    {
+      id: "video",
+      name: "video"
+    }
+  ]
+  TYPE_IDS = TYPE_MAP.map { |type_item| type_item[:name] }
+  validates :type, inclusion: { in: TYPE_IDS }
 end
